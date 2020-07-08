a12b50068292a3ed62a9d7f7d8324d2a85ad449f
tangwei <1247549534@qq.com>
2020-07-08 18:38:12 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -1,13 +1,13 @@
 import requests,json,os
 
 # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = os.environ["SERVE"]
+sever = os.environ["off"]
 
 # 填写server酱sckey,不开启server酱则不用填
 sckey = os.environ["SCKEY"]
 
 # 填入glados账号对应cookie
-cookie = os.environ["COOKIE"]
+cookie = os.environ["N4HoUIcDAnLJWkIwXEZ4LvrmFoQ"]
 
 
 def start():    
